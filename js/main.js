f4c3d863043341bf7f8a559f9409bc3ad2fbc6e9
luiz felipe <luizfelipeeeeee13@gmail.com>
2023-12-22 21:29:23 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const form = document.getElementById('contato');
-const imgconfirmar = '<img src="./imagem/confirmar.png" alt="emoji registrando" />';
-const imgerro = '<img src="./imagem/erro.png" alt="emoji de erro" />';
+const imgconfirmar = '<img src="/imagem/confirmar.png" alt="emoji registrando" />';
+const imgerro = '<img src="/imagem/erro.png" alt="emoji de erro" />';
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
-
 
     const inputNomeContato = document.getElementById('Nome-Do-Contato');
     const inputNumeroContato = document.getElementById('Numero-Do-Contato');
@@ -15,6 +15,8 @@ form.addEventListener('submit', function(e){
     linha += '</tr>';
 
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linha;
+    corpoTabela.insertAdjacentHTML('beforeend', linha);
 
-})
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+});
